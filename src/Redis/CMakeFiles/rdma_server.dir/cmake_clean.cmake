81eac3493b568c372b1f92d4b0c30ec321ab2145
Varsha Natarajan <varshanatarajan311298@gmail.com>
2023-11-26 01:42:06 -0600

    added rdma sample code

---
+++ src/Redis/CMakeFiles/rdma_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rdma_server.dir/rdma_common.c.o"
+  "CMakeFiles/rdma_server.dir/rdma_common.c.o.d"
+  "CMakeFiles/rdma_server.dir/rdma_server.c.o"
+  "CMakeFiles/rdma_server.dir/rdma_server.c.o.d"
+  "bin/rdma_server"
+  "bin/rdma_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rdma_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
